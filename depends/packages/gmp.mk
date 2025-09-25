f7b7c1d4705c758f710d570cd81869924adb617e
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2025-09-25 15:07:41 +0300

    gmp 6.3

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,38 +1,23 @@
 package=gmp
-$(package)_version=6.2.1
-$(package)_download_path=https://ftp.gnu.org/gnu/gmp/
+$(package)_version=6.3.0
+$(package)_download_path=https://ftp.gnu.org/gnu/gmp
 $(package)_file_name=gmp-$($(package)_version).tar.bz2
-$(package)_sha256_hash=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a36b4d7c
-$(package)_patches=applem1.patch
+$(package)_sha256_hash=ac28211a7cfb609bae2e2c8d6058d66c8fe96434f740cf6fe2e47b000d1c20cb
+$(package)_patches=include_ldflags_in_configure.patch
 
 define $(package)_set_vars
-$(package)_config_opts+=--enable-cxx --with-pic --disable-shared
-ifndef GUIX_ENVIRONMENT
-  $(package)_config_opts += --enable-fat
-else
-  ifneq ($(HOST),x86_64-apple-darwin)
-    $(package)_config_opts += --enable-fat
-  endif
-endif
-$(package)_config_opts_arm64_darwin+=--build=$(BUILD) --host=$(subst arm64,aarch64,$(HOST))
-$(package)_cflags_armv7l_linux+=-march=armv7-a
-$(package)_config_opts_mingw32+=CC_FOR_BUILD=gcc gmp_cv_prog_exeext_for_build=
-endef
-
-# Guix tries to use lld as the linker, but it does not support arm64-darwin
-# --disable-assembly needs to disable fat
-ifdef GUIX_ENVIRONMENT
-$(package)_cflags_darwin+=-fuse-ld=lld
-$(package)_cxxflags_darwin+=-fuse-ld=lld
-$(package)_config_opts_x86_64_darwin+=--disable-assembly
-endif
-
-define $(package)_config_cmds
-  $($(package)_autoconf)
+$(package)_config_opts += --disable-shared --enable-cxx --enable-fat
+$(package)_cflags_aarch64 += -march=armv8-a
+$(package)_cflags_armv7l += -march=armv7-a
+$(package)_cflags_x86_64 += -march=x86-64
 endef
 
 define $(package)_preprocess_cmds
-  patch -p1 <$($(package)_patch_dir)/applem1.patch
+  patch -p1 < $($(package)_patch_dir)/include_ldflags_in_configure.patch
+endef
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
@@ -41,4 +26,8 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  rm lib/*.la
 endef
